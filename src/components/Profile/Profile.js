ecd64644e461808d5a17744bb21616edba07ca21
thejsgirl <prinegi100@gmail.com>
2019-07-05 09:34:21 +0530

    feat: added buttons

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,10 +6,13 @@ class Profile extends Component {
 
     constructor(props) {
         super(props)
+        this.handleOnclick = this.handleOnclick.bind(this);
+
     }
     componentDidMount() {
         this.props.user(); 
     }
+
     render() {
         return(
             <div>
@@ -17,8 +20,20 @@ class Profile extends Component {
                 {this.props.userDetails ?<div className="profile">
                     <h1 className="greet">Helllo !
                         ! {this.props.userDetails.username}</h1>
-
                         <img src={require('./image.jpg')} />
+                        
+                          <label>Name:  {(this.props.userDetails.username).toUpperCase()}</label>
+
+                          <label>Email:  {this.props.userDetails.email}</label>
+
+                            <label>Username: {this.props.userDetails.username}</label>
+                            
+                            <div>
+
+                            <button onClick={this.handleOnclick}>Edit</button>
+                            <button>Save</button>
+                                </div>
+                        
                     </div>: ''}
                 
             </div>
